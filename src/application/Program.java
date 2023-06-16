55ad000dcdd26dc9c383a15aa694cfa4f8609f67
Marcus-Vinicius-Oliveira <mvoliveirajus@gmail.com>
2023-06-16 16:43:29 -0300

    criado novos departamentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,6 +17,8 @@ public class Program {
         try {
             conn = DB.getConnection();
 
+            /*
+
             st = conn.prepareStatement("INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
                     + "VALUES "
@@ -26,11 +28,30 @@ public class Program {
             st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
             st.setDouble(4, 3000.0);
             st.setInt(5, 4);
+             */
+
+            st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+
 
             int rowsAffected = st.executeUpdate();
 
+<<<<<<< Updated upstream
             System.out.println("Done! Rows affected: " + rowsAffected);
         }catch (SQLException | ParseException e){
+=======
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            }
+            else {
+                System.out.println("No rown affected!");
+            }
+        }catch (SQLException e){
+>>>>>>> Stashed changes
             e.printStackTrace();
             
         } finally {
